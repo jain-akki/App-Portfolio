3484ba04e45788207a28f99b6ecdec08ac0dc3aa
Akshay Jain <jain.akshay5244@gmail.com>
2016-08-14 12:52:40 +0530

    Refactoring Code remove all the redundant code & create the leapfrog structure

--- www/js/app.js
+++ www/js/app.js
@@ -1,73 +1,58 @@
-// Ionic Starter App
+(function () {
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+  'use strict';
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+  angular.module('githubPortfolio', ['ionic', 'githubPortfolio.controllers'])
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
+   .run(function ($ionicPlatform) {
+     $ionicPlatform.ready(function () {
+       
+       if (window.cordova && window.cordova.plugins.Keyboard) {
+         cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+         cordova.plugins.Keyboard.disableScroll(true);
+       }
+       if (window.StatusBar) {
+         StatusBar.styleDefault();
+       }
+     });
+   })
 
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+   .config(function ($stateProvider, $urlRouterProvider) {
 
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
+     $stateProvider
+       .state('main', {
+         url: '/main',
+         abstract: true,
+         templateUrl: 'templates/sideMenu.html',
+         controller: 'mainCtrl'
+       })
+       .state('main.gitProfile', {
+         url: '/gitProfile',
+         views: {
+           'menuContent': {
+             templateUrl: 'templates/gitProfile.html'
+           }
+         }
+       })
+       .state('main.personalProfile', {
+         url: '/personalProfile',
+         views: {
+           'menuContent': {
+             templateUrl: 'templates/personalProfile.html'
+           }
+         }
+       })
+       .state('main.aboutMe', {
+         url: '/aboutMe',
+         views: {
+           'menuContent': {
+             templateUrl: 'templates/aboutMe.html'
+           }
+         }
+       });
+     
+     $urlRouterProvider.otherwise('/main/gitProfile');
+   });
 
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
-
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
-});
+})();
+ 
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,56 +1,37 @@
-angular.module('starter.controllers', [])
+(function () {
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  'use strict';
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+  angular.module('githubPortfolio.controllers', [])
 
-  // Form data for the login modal
-  $scope.loginData = {};
+    .controller('mainCtrl', function ($scope, $ionicModal, $timeout) {
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+      $scope.loginData = {};
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+      $ionicModal.fromTemplateUrl('templates/login.html', {
+        scope: $scope
+      }).then(function (modal) {
+        $scope.modal = modal;
+      });
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+      $scope.closeLogin = function () {
+        $scope.modal.hide();
+      };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+      $scope.login = function () {
+        $scope.modal.show();
+      };
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
+      $scope.doLogin = function () {
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
+        console.log('Doing login', $scope.loginData);
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+        $timeout(function () {
+          $scope.closeLogin();
+        }, 1000);
+      };
+    });
+
+})();
+
+
